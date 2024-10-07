cab7849c85557b50a3577502974417f46cd2fdb2
loregbrw <loregobara@gmail.com>
2024-10-07 16:04:21 -0300

    feat: Treated datasheet and ran_over_strip column

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
+treated_df = pd.DataFrame()
 df = pd.read_csv("./data/telemetry-rio-5-laps.csv")
 
+treated_df["ran_over_strip"] = df["wheel_on_rumble_strip_front_left"] | df["wheel_on_rumble_strip_front_right"] | df["wheel_on_rumble_strip_rear_left"] | df["wheel_on_rumble_strip_rear_right"]
+
+treated_df.to_csv("./data/treated-datasheet.csv")
